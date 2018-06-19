eb029f4650e74bb981cc587030f33f8e0f18d844
FangLargo <dymoblade@gmail.com>
2018-06-19 18:34:23 +1200

    Init

---
+++ desktop/src/com/mf/noisemaker/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.mf.noisemaker.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mf.noisemaker.Noisemaker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		System.setProperty("org.lwjgl.opengl.Display.allowSoftwareOpenGL", "true");
+		new LwjglApplication(new Noisemaker(), config);
+	}
+}
